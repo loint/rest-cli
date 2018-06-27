e51f3f89d1120489e9b08cff0debd3afaab1f602
Loi Nguyen <loinguyentrung@gmail.com>
2018-06-27 12:25:18 +0700

    Added test case for rest cli

--- rest_test.py
+++ rest_test.py
@@ -1,17 +1,37 @@
 import pytest
-from os import system, path
+from os import system, path, getcwd, chdir
 
 __author__ = "Loi Nguyen <loinguyentrung@gmail.com>"
 
 
-class TestRest:
+class TestRestCLI:
 
     @staticmethod
     def install():
         system('make && make install')
 
-    def test_rest_create(self):
-        self.install()
+    @staticmethod
+    def assert_database():
+        assert True is True
+
+    @staticmethod
+    def assert_structure():
+        assert path.isdir('vendor') is True
+        assert path.isdir('src/Model') is True
+        assert path.isdir('src/Repository') is True
+        assert path.isdir('src/Service') is True
+        assert path.isdir('src/Controller') is True
+
+    def test_rest_cli(self):
+        #self.install()
         system('rest create user --quite --force')
         assert path.isdir('user') is True
+        current_dir = getcwd()
+        chdir(current_dir + '/user')
+        system('rest migrate')
+        self.assert_database()
+        #system('rest sync')
+        #self.assert_structure()
+        chdir(current_dir)
+
 
