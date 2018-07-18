294f710463ec8102ab54b57fb8d5b4eb4735a275
Loi Nguyen <loinguyentrung@gmail.com>
2018-07-18 15:19:25 +0700

    Removed un-used files

--- rest-cli-master/generator/__init__.py
+++
@@ -1,4 +0,0 @@
-from helper import *
-from model_template import *
-from model import *
-from route import *
--- rest-cli-master/generator/route.py
+++
@@ -1,4 +0,0 @@
-# Will be rewrite from PHP version
-class RouteGenerator:
-    def __init__(self):
-        pass
